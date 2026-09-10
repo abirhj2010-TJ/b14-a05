5a45091b3b77d77dfee939625fe5c6a1c16deeb6
abirhj2010-TJ <abirhj2010@gmail.com>
2026-09-10 21:35:03 +0600

    banner design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './Components/Banner/Banner'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
     </>
   )
 }
--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,18 +1,26 @@
+import banner_img from "../../assets/banner-stack.png";
+
 const Banner = () => {
   return (
-    <div>
+    <div className="grid grid-cols-2 justify-center items-center mt-7 container mx-auto px-20">
       <div>
-        <h2>Build Your Ideal Development Stack</h2>
-        <p>
+        <h2 className="text-6xl font-bold lg:w-xl mb-5">
+          Build Your Ideal <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E]/80 to-[#7C3AED] bg-clip-text text-transparent">Development Stack</span>
+        </h2>
+        <p className="lg:w-md mb-12">
           Explore frontend, backend, database, and tooling options, compare them
           side by side, and put together the stack that fits your next project.
         </p>
 
-        <button>Explore Technologies</button>
-        <button>Learn More</button>
+        <div className="space-x-3">
+          <button className="btn rounded-md px-7 bg-linear-to-r from-[#F97316] to-[#EC4899] text-white">Explore Technologies</button>
+          <button className="btn rounded-md px-10">Learn More</button>
+        </div>
       </div>
 
-      <div></div>
+      <div className="flex justify-end items-center">
+        <img src={banner_img} alt="" />
+      </div>
     </div>
   );
 };
