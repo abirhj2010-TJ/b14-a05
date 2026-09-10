a32f329c34fcf4ee39fccd746dc92be51e4ecf1a
abirhj2010-TJ <abirhj2010@gmail.com>
2026-09-10 20:57:42 +0600

    navbar design done

---
+++ src/Components/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+const Banner = () => {
+  return (
+    <div>
+      <div>
+        <h2>Build Your Ideal Development Stack</h2>
+        <p>
+          Explore frontend, backend, database, and tooling options, compare them
+          side by side, and put together the stack that fits your next project.
+        </p>
+
+        <button>Explore Technologies</button>
+        <button>Learn More</button>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,78 @@
+import logo from "../../assets/logo-text.png";
+import hamburger from "../../assets/hamburger.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm container mx-auto relative">
+      <div className="navbar-start">
+        <div className="dropdown md:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost p-2">
+            <img src={hamburger} alt="Menu" className="w-5 h-5 sm:w-6 sm:h-6" />
+          </div>
+
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Technologies</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <img
+          src={logo}
+          alt="Logo"
+          className="hidden md:block w-24 lg:w-28 xl:w-32"
+        />
+      </div>
+
+      <div className="absolute left-1/2 -translate-x-1/2 md:hidden">
+        <img src={logo} alt="Logo" className="w-20 sm:w-24" />
+      </div>
+
+      <div className="navbar-center hidden md:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a className="text-sm lg:text-base xl:text-base">Home</a>
+          </li>
+          <li>
+            <a className="text-sm lg:text-base xl:text-base">Technologies</a>
+          </li>
+          <li>
+            <a className="text-sm lg:text-base xl:text-base">Projects</a>
+          </li>
+          <li>
+            <a className="text-sm lg:text-base xl:text-base">About</a>
+          </li>
+          <li>
+            <a className="text-sm lg:text-base xl:text-base">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end gap-1 sm:gap-2">
+        <a className="btn btn-sm sm:btn-md text-xs sm:text-sm lg:text-md rounded-full">
+          Sign In
+        </a>
+        <a className="btn btn-sm sm:btn-md text-xs sm:text-sm lg:text-md rounded-full">
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
